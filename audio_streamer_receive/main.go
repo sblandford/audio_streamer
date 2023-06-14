46411d463b61f326650189d606baff95d38181c9
Simon Blandford <home@simonb.org.uk>
2023-06-14 10:46:57 +0100

    Added Gui on receiver

--- audio_streamer_receive/main.go
+++ audio_streamer_receive/main.go
@@ -9,6 +9,14 @@ import (
 	"time"
 
 	"github.com/gordonklaus/portaudio"
+
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 const channels = 2
@@ -19,7 +27,6 @@ const audioBufferLength = packetSize / wordLength
 const sampleRate = 48000
 const missReportRateSec = 10
 
-var quit = make(chan bool)
 var b buffers
 
 
@@ -31,10 +38,10 @@ type buffers struct {
 	missCount	float32
 }
 
+var quitPlayOut = make(chan bool)
 func playOut() {
-	chk(portaudio.Initialize())
-	defer portaudio.Terminate()
 
+	chk(portaudio.Initialize())
 
 	stream, err := portaudio.OpenDefaultStream(0, channels, sampleRate, audioBufferLength / channels, func(out []int16) {
 		select {
@@ -51,19 +58,21 @@ func playOut() {
 		}
 	})
 	chk(err)
+
 	chk(stream.Start())
 
-	<-quit
+	<-quitPlayOut
 
 	stream.Close()
 	portaudio.Terminate()
 	println("Audio terminated")
 }
 
+var quitProcessPackets = make(chan bool)
 func processPackets () {
 	for {
 		select {
-		case <-quit:
+		case <- quitProcessPackets:
 			println("Processing stopped")
 			return
 		case buf := <- b.dataBuffer:
@@ -77,6 +86,7 @@ func processPackets () {
 	}
 }
 
+var quitFetchPackets = make(chan bool)
 func fetchPackets() {
 
 	addr, err := net.ResolveUDPAddr("udp4", "224.0.0.3:1234")
@@ -87,7 +97,7 @@ func fetchPackets() {
 
 	for {
 		select {
-		case <-quit:
+		case <- quitFetchPackets:
 			conn.Close()
 			println("Connection closed")
 			return
@@ -100,17 +110,22 @@ func fetchPackets() {
 	}
 }
 
-func counter() {
+var quitMissCounter = make(chan bool)
+func MissCounter(statsText *widget.Label) {
 	for {
 		select {
-		case <-quit:
+		case <- quitMissCounter:
 			return
 		default:
-			time.Sleep(missReportRateSec * time.Second)
-			missPercent := (b.missCount * 100) / (b.hitCount + b.missCount)
-			fmt.Printf("Error rate: %06.3f%%\n", missPercent)
-			b.hitCount = 0
-			b.missCount = 0
+			if (int64(time.Now().Unix()) % missReportRateSec) == 0 {
+				missPercent := (b.missCount * 100) / (b.hitCount + b.missCount)
+				stats := fmt.Sprintf("Error rate: %06.3f%%", missPercent)
+				fmt.Println(stats)
+				statsText.SetText(stats)
+				b.hitCount = 0
+				b.missCount = 0
+			}
+			time.Sleep(1 * time.Second)
 		}
 	}
 }
@@ -119,24 +134,56 @@ func main() {
 	b.dataBuffer = make(chan []byte, dataBufferLength)
 	b.audioBuffer = make(chan []int16, 300)
 
-	var end = make(chan struct{})
+	end := make(chan struct{})
+	c := make(chan os.Signal, 1)
+
+	myApp := app.New()
+	myWindow := myApp.NewWindow("UDP Audio Receiver")
+
+	 // Define a welcome text centered
+	 text := canvas.NewText("Audio Receiver", color.White)
+	 text.Alignment = fyne.TextAlignCenter
+
+	 statsText := widget.NewLabel("")
+	 statsText.SetText("---")
+
+
+    // Display a vertical box containing text and error stats
+    box := container.NewVBox(
+        text,
+		statsText,
+    )
+
+	// Trap Window Close
+	myWindow.SetCloseIntercept(func() {
+		c <- syscall.SIGTERM
+	})
+
+	// Display our content
+	myWindow.SetContent(box)
 
 	go fetchPackets()
 	go processPackets()
 	go playOut()
-	go counter ()
+	go MissCounter(statsText)
 
-	c := make(chan os.Signal, 1)
+
 	signal.Notify(c, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-c
-		fmt.Println("Exiting")
-		quit <- true
-
+		fmt.Printf("\nExiting\n")
+		quitMissCounter <- true
+		quitFetchPackets <- true
+		quitProcessPackets <- true
+		quitPlayOut <- true
 		time.Sleep(1 * time.Second)
 		close(end)
 		os.Exit(0)
 	}()
+
+	// Show window and run app
+	myWindow.ShowAndRun()
+
 	<-end
 }
 
